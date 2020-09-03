7ad023b48a28d3967008e82e35071e990c22c6b3
TrevorTanSinCho <xingzhu89@gmail.com>
2020-09-03 16:15:27 +0800

    section 3 video 1 RecycleView

---
+++ app/src/main/java/com/project/msgshareapp/Model.kt
@@ -0,0 +1,32 @@
+package com.project.msgshareapp
+
+data class Hobby (var title:String)
+
+object Supplier {
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Jogging"),
+        Hobby("Talking"),
+        Hobby("Cooking"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Jogging"),
+        Hobby("Talking"),
+        Hobby("Cooking"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Walking"),
+        Hobby("Sleeping"),
+        Hobby("Swimming"),
+        Hobby("Jogging"),
+        Hobby("Talking"),
+        Hobby("Cooking"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Walking"),
+        Hobby("Sleeping")
+    )
+}
